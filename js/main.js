5c9250c61b1e3709e1abd67be5af5e17fad6f99f
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-20 19:36:21 +0100

    feat: bonus 1 e 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Array immagini
+
+const memberPhotos = ['wayne-barnett-founder-ceo.jpg', 'angela-caroll-chief-editor.jpg', 'walter-gordon-office-manager.jpg', 'angela-lopez-social-media-manager.jpg', 'scott-estrada-developer.jpg', 'barbara-ramos-graphic-designer.jpg'];
+
 // Array membri del team
 
 const team = [
@@ -43,11 +47,24 @@ const teamContainer = document.querySelector('.team');
 
 for (let i = 0; i < team.length; i++) {
     let card = document.createElement('div');
-    teamContainer.append(card);
-    card.append(`
-    ${team[i].nome} -
-    ${team[i].ruolo} -
-    ${team[i].foto}`);
+    const img = document.createElement('img');
+    const nome = document.createElement('div');
+    const ruolo = document.createElement('div');
+    img.src = `img/${memberPhotos[i]}`;
+    img.alt = `Member photo ${memberPhotos[i]}`;
 
+    card.classList.add('card', 'text-center');
+    card.append(img);
+
+    nome.classList.add('mt-3', 'fw-bold')
+    nome.append(`${team[i].nome}`);
+    ruolo.classList.add('mb-3');
+    ruolo.append(`${team[i].ruolo}`);
+
+    teamContainer.append(card);
+    card.append(nome);
+    card.append(ruolo);
+
+    console.log(card);
     console.log(team[i]);
 };
